70d883dc1331c15a3297c6f8cdabb5f560c952bd
Alexandre Vassard <vassard.alexandre@gmail.com>
2022-09-26 17:28:57 +0200

    Changed base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,60 +1,60 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'IMAP mailbox',
-  tagline: 'Simple NodeJS IMAP mailbox library',
-  url: 'https://vsdparibas.github.io',
-  baseUrl: '/imap-mailbox-doc/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  title: "IMAP mailbox",
+  tagline: "Simple NodeJS IMAP mailbox library",
+  url: "https://imap-mailbox.js.org",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Vsdparibas', // Usually your GitHub org/user name.
-  projectName: 'imap-mailbox-doc', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "Vsdparibas", // Usually your GitHub org/user name.
+  projectName: "imap-mailbox-doc", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'fr'],
+    defaultLocale: "en",
+    locales: ["en", "fr"],
     localeConfigs: {
       en: {
-        label: 'English',
-        htmlLang: 'en-US'
+        label: "English",
+        htmlLang: "en-US",
       },
       fr: {
-        label: 'Français',
-        htmlLang: 'fr'
-      }
-    }
+        label: "Français",
+        htmlLang: "fr",
+      },
+    },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Vsdparibas/imap-mailbox-doc/tree/master/',
+            "https://github.com/Vsdparibas/imap-mailbox-doc/tree/master/",
           remarkPlugins: [
-            [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
           ],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -64,48 +64,48 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'IMAP mailbox',
+        title: "IMAP mailbox",
         items: [
           {
-            type: 'doc',
-            docId: 'tutorial/intro',
-            position: 'left',
-            label: 'Tutorial',
+            type: "doc",
+            docId: "tutorial/intro",
+            position: "left",
+            label: "Tutorial",
           },
           {
-            href: '/docs/category/documentation',
-            position: 'left',
-            label: 'Documentation',
+            href: "/docs/category/documentation",
+            position: "left",
+            label: "Documentation",
           },
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
           {
-            href: 'https://github.com/Vsdparibas/imap-mailbox/',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/Vsdparibas/imap-mailbox/",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/tutorial/intro',
+                label: "Tutorial",
+                to: "/docs/tutorial/intro",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
